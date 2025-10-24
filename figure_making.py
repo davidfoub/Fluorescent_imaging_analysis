7f88c38ba762f2518be36a2529a87fa45fbd1bed
superwhat1 <david.foubertl@gmail.com>
2025-10-24 14:31:49 -0400

    old figure making changes

--- figure_making.py
+++ figure_making.py
@@ -51,65 +51,66 @@ mpl.rcParams["axes.facecolor"]='k'
 mpl.rcParams["axes.edgecolor"]=colour
 mpl.rcParams["figure.edgecolor"]=colour
 mpl.rcParams["figure.facecolor"]='k'
-mpl.rcParams["font.size"]=15
+mpl.rcParams["font.size"]=20
 palette = ["#1be6ec", "#e51bec"]
 
 #Figures for glia plasticity
 #GLIA DATA
+
+responsive_glia_data= pd.read_csv("E:/glia projects/plasticity/summaries/responding_glia_count.csv").sort_values(by=['time group']).sort_values(by=['treatment'],ascending=False)
+responsive_glia_data=responsive_glia_data.loc[~((responsive_glia_data['time group']==30) | (pd.isna(responsive_glia_data['time group'])))]
+
+glia_transients_count_data= pd.read_csv("E:/glia projects/plasticity/summaries/glia_transient_count.csv").sort_values(by=['time group']).sort_values(by=['treatment'],ascending=False)
+glia_transients_count_data=glia_transients_count_data.loc[~((glia_transients_count_data['time group']==30) | (pd.isna(glia_transients_count_data['time group'])))]
+
+glia_transients_data= pd.read_csv("E:/glia projects/plasticity/summaries/glia_training_glia_summary_by_cell.csv").sort_values(by=['time group']).sort_values(by=['treatment'],ascending=False)
+glia_transients_data=glia_transients_data.loc[~((glia_transients_data['time group']==30) | (pd.isna(glia_transients_data['time group'])))]
+
     #no training
-fig,axes =plt.subplots(nrows=2, ncols=4, figsize = (25,10), layout="constrained")
+fig,axes =plt.subplots(nrows=1, ncols=2, figsize = (25,10), layout="constrained")
 sns.set_palette(palette)
 
-responsive_glia_data= pd.read_csv("E:/glia projects/plasticity/summaries/glia_training_glia_responding_cell_numbers.csv").sort_values(by=['time group']).replace(to_replace='min99',value='min100')
-responsive_glia_data=responsive_glia_data.loc[~((responsive_glia_data['time group']=='min30') | (pd.isna(responsive_glia_data['time group'])))]
-responsive_glia_ctrl = sns.pointplot(x = "time group", y = "Corrected cell count", hue = "treatment", data=responsive_glia_data[responsive_glia_data['treatment'].str.contains('without training')], errorbar='se', capsize=0.2, ax=axes[0,0])
-responsive_glia_ctrl.set_ylim(top=max(responsive_glia_data["Corrected cell count"])*1.2, bottom=min(responsive_glia_data["Corrected cell count"])*0.6)
-responsive_glia_ctrl.set_title("Active glia count - no training")
+responsive_glia_ctrl = sns.pointplot(x = "time group", y = "Corrected cell count", hue = "treatment", data=responsive_glia_data[responsive_glia_data['treatment'].str.contains('without training')], errorbar='se', capsize=0.2, ax=axes[0])
+#responsive_glia_ctrl.set_ylim(top=max(responsive_glia_data["Corrected cell count"])*1.2, bottom=min(responsive_glia_data["Corrected cell count"])*0.6)
+responsive_glia_ctrl.set_title("Active glia count")
 sns.stripplot(x = "time group", y = "Corrected cell count", data = responsive_glia_data[responsive_glia_data['treatment'].str.contains('without training')], size=5, hue="treatment", dodge=False, legend=False, ax=responsive_glia_ctrl)
 
+glia_transients_count_ctrl = sns.pointplot(x = "time group", y = "normalized response count", hue = "treatment", data=glia_transients_count_data[glia_transients_count_data['treatment'].str.contains('without training')], errorbar='se', capsize=0.2, legend=False, ax=axes[1])
+#glia_transients_count_ctrl.set_ylim(top=max(glia_transients_count_data["normalized response count"])*1.2, bottom=min(glia_transients_count_data["normalized response count"])*0.6)
+glia_transients_count_ctrl.set_title("Glia transients counts")
+sns.stripplot(x = "time group", y = "normalized response count", data = glia_transients_count_data[glia_transients_count_data['treatment'].str.contains('without training')], size=5, hue="treatment", dodge=False, legend=False, ax=glia_transients_count_ctrl)
+'''
+glia_transients_amp_ctrl = sns.pointplot(x = "time group", y = "peaks", hue = "treatment", data=glia_transients_data[glia_transients_data['treatment'].str.contains('without training')], errorbar='se', capsize=0.2, ax=axes[0,2])
+glia_transients_amp_ctrl.set_ylim(top=max(glia_transients_data["peaks"])*1.2, bottom=min(glia_transients_data["peaks"])*0.6)
+glia_transients_amp_ctrl.set_title("Glia peak amplitude - no training")
+sns.stripplot(x = "time group", y = "peaks", data = glia_transients_data[glia_transients_data['treatment'].str.contains('without training')], size=5, hue="treatment", dodge=False, legend=False, ax=glia_transients_amp_ctrl)
+
+glia_transients_auc_ctrl = sns.pointplot(x = "time group", y = "area", hue = "treatment", data=glia_transients_data[glia_transients_data['treatment'].str.contains('without training')], errorbar='se', capsize=0.2, ax=axes[0,3])
+glia_transients_auc_ctrl.set_ylim(top=max(glia_transients_data["area"])*1.2, bottom=min(glia_transients_data["area"])*0.6)
+glia_transients_auc_ctrl.set_title("Glia auc - no training")
+sns.stripplot(x = "time group", y = "area", data = glia_transients_data[glia_transients_data['treatment'].str.contains('without training')], size=5, hue="treatment", dodge=False, legend=False, ax=glia_transients_auc_ctrl)
+
+	#With training
 responsive_glia_exp= sns.pointplot(x = "time group", y = "Corrected cell count", hue = "treatment", data=responsive_glia_data[responsive_glia_data['treatment'].str.contains('with training')], errorbar='se', capsize=0.2, ax=axes[1,0])
 responsive_glia_exp.set_ylim(top=max(responsive_glia_data["Corrected cell count"])*1.2, bottom=min(responsive_glia_data["Corrected cell count"])*0.6)
 responsive_glia_exp.set_title("Active glia count - training")
 sns.stripplot(x = "time group", y = "Corrected cell count", data = responsive_glia_data[responsive_glia_data['treatment'].str.contains('with training')], size=5, hue="treatment", dodge=False, legend=False, ax=responsive_glia_exp)
 
-
-glia_transients_count_data= pd.read_csv("E:/glia projects/plasticity/summaries/glia_training_glia_response_numbers.csv").sort_values(by=['time group']).replace(to_replace='min99',value='min100')
-glia_transients_count_data=glia_transients_count_data.loc[~((glia_transients_count_data['time group']=='min30') | (pd.isna(glia_transients_count_data['time group'])))]
-glia_transients_count_ctrl = sns.pointplot(x = "time group", y = "normalized response count", hue = "treatment", data=glia_transients_count_data[glia_transients_count_data['treatment'].str.contains('without training')], errorbar='se', capsize=0.2, ax=axes[0,1])
-glia_transients_count_ctrl.set_ylim(top=max(glia_transients_count_data["normalized response count"])*1.2, bottom=min(glia_transients_count_data["normalized response count"])*0.6)
-glia_transients_count_ctrl.set_title("Glia transients counts - no training")
-sns.stripplot(x = "time group", y = "normalized response count", data = glia_transients_count_data[glia_transients_count_data['treatment'].str.contains('without training')], size=5, hue="treatment", dodge=False, legend=False, ax=glia_transients_count_ctrl)
-	
-	#With training
 glia_transients_count_exp= sns.pointplot(x = "time group", y = "normalized response count", hue = "treatment", data=glia_transients_count_data[glia_transients_count_data['treatment'].str.contains('with training')], errorbar='se', capsize=0.2, ax=axes[1,1])
 glia_transients_count_exp.set_ylim(top=max(glia_transients_count_data["normalized response count"])*1.2, bottom=min(glia_transients_count_data["normalized response count"])*0.6)
 glia_transients_count_exp.set_title("Glia transients counts - training")
 sns.stripplot(x = "time group", y = "normalized response count", data = glia_transients_count_data[glia_transients_count_data['treatment'].str.contains('with training')], size=5, hue="treatment", dodge=False, legend=False, ax=glia_transients_count_exp)
-
-glia_transients_data= pd.read_csv("E:/glia projects/plasticity/summaries/glia_training_glia_summary_by_cell.csv").sort_values(by=['time group']).replace(to_replace='min99',value='min100')
-glia_transients_data=glia_transients_data.loc[~((glia_transients_data['time group']=='min30') | (pd.isna(glia_transients_data['time group'])))]
-#glia_transients_amp_ctrl = sns.pointplot(x = "time group", y = "peaks", hue = "treatment", data=glia_transients_data[glia_transients_data['treatment'].str.contains('without training')], errorbar='se', capsize=0.2, ax=axes[0,2])
-#glia_transients_amp_ctrl.set_ylim(top=max(glia_transients_data["peaks"])*1.2, bottom=min(glia_transients_data["peaks"])*0.6)
-#glia_transients_amp_ctrl.set_title("Glia peak amplitude - no training")
-#sns.stripplot(x = "time group", y = "peaks", data = glia_transients_data[glia_transients_data['treatment'].str.contains('without training')], size=5, hue="treatment", dodge=False, legend=False, ax=glia_transients_amp_ctrl)
 
 glia_transients_amp_exp= sns.pointplot(x = "time group", y = "peaks", hue = "treatment", data=glia_transients_data[glia_transients_data['treatment'].str.contains('with training')], errorbar='se', capsize=0.2, ax=axes[1,2])
 glia_transients_amp_exp.set_ylim(top=max(glia_transients_data["peaks"])*1.2, bottom=min(glia_transients_data["peaks"])-min(glia_transients_data["peaks"])*1.2)
 glia_transients_amp_exp.set_title("Glia peak amplitude - training")
 sns.stripplot(x = "time group", y = "peaks", data = glia_transients_data[glia_transients_data['treatment'].str.contains('with training')], size=5, hue="treatment", dodge=False, legend=False, ax=glia_transients_amp_exp)
 
-glia_transients_data= pd.read_csv("E:/glia projects/plasticity/summaries/glia_training_glia_summary_by_cell.csv").sort_values(by=['time group']).replace(to_replace='min99',value='min100')
-glia_transients_data=glia_transients_data.loc[~((glia_transients_data['time group']=='min30') | (pd.isna(glia_transients_data['time group'])))]
-#glia_transients_amp_ctrl = sns.pointplot(x = "time group", y = "area", hue = "treatment", data=glia_transients_data[glia_transients_data['treatment'].str.contains('without training')], errorbar='se', capsize=0.2, ax=axes[0,3])
-#glia_transients_amp_ctrl.set_ylim(top=max(glia_transients_data["area"])*1.2, bottom=min(glia_transients_data["area"])*0.6)
-#glia_transients_amp_ctrl.set_title("Glia auc - no training")
-#sns.stripplot(x = "time group", y = "area", data = glia_transients_data[glia_transients_data['treatment'].str.contains('without training')], size=5, hue="treatment", dodge=False, legend=False, ax=glia_transients_amp_ctrl)
-
-glia_transients_amp_exp= sns.pointplot(x = "time group", y = "area", hue = "treatment", data=glia_transients_data[glia_transients_data['treatment'].str.contains('with training')], errorbar='se', capsize=0.2, ax=axes[1,3])
-glia_transients_amp_exp.set_ylim(top=max(glia_transients_data["area"])*1.2, bottom=min(glia_transients_data["area"])-min(glia_transients_data["area"])*1.2)
-glia_transients_amp_exp.set_title("Glia auc - training")
-sns.stripplot(x = "time group", y = "area", data = glia_transients_data[glia_transients_data['treatment'].str.contains('with training')], size=5, hue="treatment", dodge=False, legend=False, ax=glia_transients_amp_exp)
-
+glia_transients_auc_exp= sns.pointplot(x = "time group", y = "area", hue = "treatment", data=glia_transients_data[glia_transients_data['treatment'].str.contains('with training')], errorbar='se', capsize=0.2, ax=axes[1,3])
+glia_transients_auc_exp.set_ylim(top=max(glia_transients_data["area"])*1.2, bottom=min(glia_transients_data["area"])-min(glia_transients_data["area"])*1.2)
+glia_transients_auc_exp.set_title("Glia auc - training")
+sns.stripplot(x = "time group", y = "area", data = glia_transients_data[glia_transients_data['treatment'].str.contains('with training')], size=5, hue="treatment", dodge=False, legend=False, ax=glia_transients_auc_exp)
+'''
 plt.show()
 
 
